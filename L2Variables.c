e3f81fd13307e8a64c71d795fbc271ecca0adb1c
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-04-07 14:27:22 +0530

    Add files via upload

---
+++ L2Variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a = 4;
+    // int b = 8.5 This is wrong because 8.5 is not a Integer
+    float b = 8.5;
+    char c = 'u';
+    int d = 69;
+
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %f \n", b);
+    printf("The value of c is %c \n", c);
+    printf("The sum of a&d is %d \n \n \n", a + d);
+
+    return 0;
+}
