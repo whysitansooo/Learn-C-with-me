86ea282e16b079b7198147328021ae5dc5ae582c
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-04-10 00:04:39 +0530

    Type declaration instructions

---
+++ L4Instructions.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    // int a = 4,b,c; //Type declaration instruction
+
+    float a = 1.1;
+    float b = a+8.9;
+    printf("The value of b is %f",b);
+    return 0;
+
+}
