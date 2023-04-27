c2ec10906fc5437b176b47636c675410fe816f05
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-04-28 00:58:30 +0530

    Learn about typedef

---
+++ L26TypeDef.c
@@ -0,0 +1,12 @@
+//cow  "typedef" 
+
+#include <stdio.h>
+typedef float realNumber;
+typedef int Integer;
+
+int main(){
+    Integer x = 50;
+    realNumber y = 10.24; 
+    printf("%d\n",x);
+    printf("%f\n",y);
+}
